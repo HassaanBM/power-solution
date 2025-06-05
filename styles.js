b3ddc8fbbc5b42bf9fff2ac57b4d7e9dace20c97
Hassaan_BM <hassaanbinmasham@gmail.com>
2025-06-06 01:55:55 +0500

    Testing Loco on IOS

---
+++ styles.js
@@ -0,0 +1,13 @@
+console.log("Styles.js loaded")
+
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  smartphone: {
+    smooth: true
+  },
+  tablet: {
+    smooth: true
+  }
+});

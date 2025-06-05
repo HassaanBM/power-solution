b6c4d4cbe677677480ba5eaea38a0ee2443fc73c
Hassaan_BM <hassaanbinmasham@gmail.com>
2025-06-06 03:03:57 +0500

    Changing base library.

--- styles.js
+++ styles.js
@@ -1,13 +1,6 @@
 console.log("Styles.js loaded")
 
-
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  smartphone: {
-    smooth: false
-  },
-  tablet: {
-    smooth: false
-  }
+ScrollSmoother.create({
+  smooth: 1,
+  effects: true,
 });

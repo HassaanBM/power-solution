dddf61250303d5642a46e65fd7bbc037ec61f153
Hassaan_BM <hassaanbinmasham@gmail.com>
2025-06-06 03:14:55 +0500

    Disabled tial warning.

--- styles.js
+++ styles.js
@@ -1,6 +1,14 @@
 console.log("Styles.js loaded")
 
-ScrollSmoother.create({
-  smooth: 1,
-  effects: true,
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+// Disable the trail warning
+gsap.config({trialWarn: false});
+
+
+const smoother = ScrollSmoother.create({
+  wrapper: "#smooth-wrapper",
+  content: "#smooth-content",
+  smooth: 1.5, // seconds it takes to "catch up"
+  effects: true // enables parallax effects
 });

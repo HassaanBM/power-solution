390ea0982d0fa2ecdea11241e86faef354852eb4
Hassaan_BM <hassaanbinmasham@gmail.com>
2025-06-06 02:55:03 +0500

    updating javascript.

--- styles.js
+++ styles.js
@@ -5,9 +5,9 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
   smartphone: {
-    smooth: true
+    smooth: false
   },
   tablet: {
-    smooth: true
+    smooth: false
   }
 });

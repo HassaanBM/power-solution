581fb052b8e933e8c46b5ec3ad0968738f33120f
Hassaan_BM <hassaanbinmasham@gmail.com>
2025-06-11 22:08:55 +0500

    Updated the sticky aside navigation to be more responsive and visually appealing..

--- styles.js
+++ styles.js
@@ -66,5 +66,37 @@ ScrollTrigger.create({
   toggleClass: "sticky-aside-active",      // Toggle class on pin
   toggleActions: "play none none reverse", // Play animation on pin, reverse on unpin
   scrub: true,                             // Tie animation to scroll position
-  markers: true                           // Set to true for debugging
+  markers: false                           // Set to true for debugging
 });
+
+
+
+// Detect if a link's href goes to the current page
+function getSamePageAnchor (link) {
+  if (
+    link.protocol !== window.location.protocol ||
+    link.host !== window.location.host ||
+    link.pathname !== window.location.pathname ||
+    link.search !== window.location.search
+  ) {
+    return false;
+  }
+  return link.hash;
+}
+// Scroll to a given hash, preventing the event given if there is one
+function scrollToHash(hash, e) {
+  const elem = hash ? document.querySelector(hash) : false;
+  if(elem) {
+    if(e) e.preventDefault();
+    gsap.to(window, {scrollTo: elem});
+  }
+}
+// If a link's href is within the current page, scroll to it instead
+document.querySelectorAll('a[href]').forEach(a => {
+  a.addEventListener('click', e => {
+    scrollToHash(getSamePageAnchor(a), e);
+  });
+});
+
+// Scroll to the element in the URL's hash on load
+scrollToHash(window.location.hash);
